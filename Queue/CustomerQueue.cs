9ad96ee660183c4b269660e39373f23aef46a42d
Martin <martin.rostejnsky@email.cz>
2023-01-14 00:42:03 +0100

    Přidejte soubory projektu.

---
+++ Queue/CustomerQueue.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue
+{
+    class CustomerQueue
+    {
+        protected Queue<int> q = new Queue<int>();
+        public int QueueLength { get { return q.Count; } }
+        public int QueueCapacity { get; set; } = 1000;
+
+        public int Arrive()
+        {
+            if (QueueLength == 1000)
+            {
+                throw new Exception("Queue is full!");
+            }
+
+            int num;
+            do
+            {
+                num = Random.Shared.Next(0, 1000);
+            } while (q.Contains(num));
+
+            q.Enqueue(num);
+            return num;
+        }
+
+        public int Serve()
+        {
+            if (QueueLength == 0)
+            {
+                throw new Exception("Queue is empty!");
+            }
+            else
+            {
+                return q.Dequeue();
+            }
+        }
+    }
+}
---
+++ Queue/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Queue;
+
+CustomerQueue q = new CustomerQueue();
+try
+{
+    for (int i = 0; i < 1001; i++)
+    {
+    Console.WriteLine("New customer (no."+q.Arrive()+")");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex);
+}
+
+try
+{
+    for (int i = 0; i < 1001; i++)
+    {
+        Console.WriteLine("New customer no." + q.Serve());
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex);
+}
